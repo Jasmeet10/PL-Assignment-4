4b165e66e317b38a08b3f84d23938a01f0cc6e63
Jasmeet Kaur Dua <57971182+Jasmeet10@users.noreply.github.com>
2020-11-15 17:55:26 -0500

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -4,4 +4,4 @@ replace :: Int -> Int -> [Int] -> [Int]
 --replace x y [] = []
 replace x y (k:ks) 
         |x == k      = y:ks
-        |otherwise   = replace x y (ks)
+        |otherwise   = k : replace x y (ks)
