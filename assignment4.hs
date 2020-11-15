bc6e021d3ac7239e138c02ce9c71cb2924280fee
Jasmeet Kaur Dua <57971182+Jasmeet10@users.noreply.github.com>
2020-11-15 17:03:00 -0500

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -2,6 +2,6 @@
 
 replace :: Int -> Int -> [a] -> [a]
 replace x y [] = []
-maxlist x y (k:ks) 
+maxlist x y (k:xs) 
         |x == k      =y
-        |otherwise   = replace x y(ks)
+        |otherwise   = replace x y(xs)
