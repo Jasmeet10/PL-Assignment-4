38e22f683557271041328d68f3832780838c1427
Jasmeet Kaur Dua <57971182+Jasmeet10@users.noreply.github.com>
2020-11-15 17:49:15 -0500

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -1,7 +1,7 @@
 --Question 1:
 
-replace :: Int -> Int -> [Int] -> Int
+replace :: Int -> Int -> [Int] -> [Int]
 --replace x y [] = []
 replace x y (k:ks) 
-        |x == k      =y
+        |x == k      =[y:ks]
         |otherwise   = replace x y (ks)
