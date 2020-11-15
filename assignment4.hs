65616aaeacb666b35ce50936d2d4829a5fe3e7de
Jasmeet Kaur Dua <57971182+Jasmeet10@users.noreply.github.com>
2020-11-15 18:27:10 -0500

    Update assignment4.hs

--- assignment4.hs
+++ assignment4.hs
@@ -5,3 +5,17 @@ replace x y [] = []
 replace x y (k:ks) 
         |x == k      = y : replace x y (ks)
         |otherwise   = k : replace x y (ks)
+        
+        
+ --Question 2:
+ 
+deletekth :: Int -> Int -> [a] -> [a]
+deletekth k n [] = []
+deletekth k n (x:xs)
+    | n == 1 = deletekth k k xs
+    | n > 1 = x : deletekth k (n-1) xs
+
+delete :: Int -> [a] -> [a]
+delete _ [] = []
+delete 1 (_:_) = []
+delete k lt = deletekth k k lt
